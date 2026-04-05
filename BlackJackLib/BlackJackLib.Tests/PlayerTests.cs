285e1d862b9a7a9adaffc4ed528f387cb67880bd
Clonyx1 <korinekluky@proton.me>
2026-04-05 22:45:08 +0200

    Add status and game logic methods to Player, add tests for PlaceBet

---
+++ BlackJackLib/BlackJackLib.Tests/PlayerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BlackJackLib.Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void PlaceBet_VariousValues_ShouldChangeBalanceAndSetBet()
+        {
+            //Arrange
+            decimal playerBalance = 150;
+            decimal betValue = 45.5m;
+
+            decimal expectedBalance = 104.5m;
+            decimal expectedBet = 45.5m;
+
+            Player player = new Player(playerBalance);
+
+            //Act
+            player.PlaceBet(betValue);
+
+            //Assert
+            Assert.Equal(expectedBalance, player.Balance);
+            Assert.Equal(expectedBet, player.Bet);
+        }
+
+        [Theory]
+        [InlineData(150, 150.7)]
+        [InlineData(150, -15)]
+        public void PlaceBet_ShouldThrowArgumentException(decimal playerBalance, decimal betValue)
+        {
+            //Arrange
+            Player player = new Player(playerBalance);
+
+            //Act & Asset
+            Assert.Throws<ArgumentException>(() => player.PlaceBet(betValue));
+        }
+    }
+}
--- BlackJackLib/BlackJackLib/Player.cs
+++ BlackJackLib/BlackJackLib/Player.cs
@@ -6,6 +6,73 @@ namespace BlackJackLib
 {
     public class Player
     {
+        public Hand Hand { get;} = new Hand();
+        public decimal Balance { get; private set; }
+        public decimal Bet { get; private set; }
 
+        public Player(decimal balance)
+        {
+            Balance = balance;
+        }
+
+        public bool CanHit()
+        {
+            if (Hand.GetTotalValue() < 21) return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Returns true if player's hand is over 21
+        /// </summary>
+        /// <returns></returns>
+        public bool IsBusted()
+        {
+            if (Hand.GetTotalValue() > 21) return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Returns true if player's hand equals 21
+        /// </summary>
+        /// <returns></returns>
+        public bool HasBlackJack()
+        {
+            if(Hand.GetTotalValue() == 21) return true;
+
+            return false;
+        }
+
+        public void PlaceBet(decimal betValue)
+        {
+            if (betValue <= 0) throw new ArgumentException("Bet must be positive");
+            if (Balance < betValue) throw new ArgumentException("Insufficient balance");
+
+            Bet += betValue;
+            Balance -= betValue;
+        }
+        /// <summary>
+        /// Adds player's bet * multiplier to player's balance, sets bet to 0
+        /// </summary>
+        /// <param name="multiplier"></param>
+        public void Win(decimal multiplier)
+        {
+            Balance += Bet * multiplier;
+            Bet = 0;
+        }
+        /// <summary>
+        /// Returns player's bet to balance, sets bet to 0
+        /// </summary>
+        public void Push()
+        {
+            Balance += Bet;
+            Bet = 0;
+        }
+        /// <summary>
+        /// Sets bet to 0
+        /// </summary>
+        public void Lose()
+        {
+            Bet = 0;
+        }
     }
 }
